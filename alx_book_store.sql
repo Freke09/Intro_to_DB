8b335aa7f7556fec9cdf05d47d96dadbb47a6744
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-08-15 11:19:19 +0100

    Modified the Database alx_book_stores.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id PRIMARY KEY,
     order_id,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    book_id,
+    book_id INT,
     FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE,
 );
