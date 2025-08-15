7087a8e3a6021b181c5a2b97f0659098abe69e90
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-08-15 11:15:09 +0100

    Modified the Database alx_book_stores.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE IF NOT EXISTS books (
+CREATE TABLE IF NOT EXISTS Books (
     book_id PRIMARY KEY,
     title VARCHAR(130),
     author_id,
@@ -10,30 +10,30 @@ CREATE TABLE IF NOT EXISTS books (
     publication_date DATE
 );
 
-CREATE TABLE IF NOT EXISTS authos(
+CREATE TABLE IF NOT EXISTS Authors(
     author_id PRIMARY KEY,
     author_name VARCHAR(215)
 );
 
-CREATE TABLE IF NOT EXISTS customers (
+CREATE TABLE IF NOT EXISTS Customers (
     customer_id PRIMARY KEY,
     customer_name VARCHAR(215),
     email VARCHAR(215),
     address TEXT
 );
 
-CREATE TABLE IF NOT EXISTS orders (
+CREATE TABLE IF NOT EXISTS Orders (
     order_id PRIMARY KEY,
     customer_id,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 );
 
-CREATE TABLE IF NOT EXISTS order_details (
+CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id PRIMARY KEY,
     order_id,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     book_id,
-    FOREIGN KEY (book_id) REFERENCES books(book_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE,
 );
