5232be393924f9e1afabdcdb707a8ff0819c1bfa
Ndifreke Ndue <ndifrekeh@gmail.com>
2025-08-15 13:44:17 +0100

    Modified task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,5 +1,5 @@
 INSERT INTO customers (customer_id, customer_name, email, address)
 VALUES  (1),
-        ("COle Baidoo"),
+        ("Cole Baidoo"),
         ("cbaidoo@sandtech.com"),
-        ("123 Hapiness Ave.");
+        ("123 Happiness Ave.");
